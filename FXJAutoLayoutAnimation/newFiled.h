42581ad2bbfc51e3aead6d8ad68c421555ab607f
fxjcs <you@example.com>
2016-04-15 14:26:02 +0800

    新建文件

---
+++ FXJAutoLayoutAnimation/newFiled.h
@@ -0,0 +1,13 @@
+//
+//  newFiled.h
+//  FXJAutoLayoutAnimation
+//
+//  Created by myApplePro01 on 16/4/15.
+//  Copyright © 2016年 LSH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface newFiled : UIView
+
+@end
---
+++ FXJAutoLayoutAnimation/newFiled.m
@@ -0,0 +1,21 @@
+//
+//  newFiled.m
+//  FXJAutoLayoutAnimation
+//
+//  Created by myApplePro01 on 16/4/15.
+//  Copyright © 2016年 LSH. All rights reserved.
+//
+
+#import "newFiled.h"
+
+@implementation newFiled
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
