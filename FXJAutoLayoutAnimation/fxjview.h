65d645542a5eb7bc117c1e3bf651b6174ff483b2
fxjcs <you@example.com>
2016-04-15 14:44:20 +0800

    fxj创建

---
+++ FXJAutoLayoutAnimation/fxjview.h
@@ -0,0 +1,13 @@
+//
+//  fxjview.h
+//  FXJAutoLayoutAnimation
+//
+//  Created by myApplePro01 on 16/4/15.
+//  Copyright © 2016年 LSH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface fxjview : UIView
+
+@end
---
+++ FXJAutoLayoutAnimation/fxjview.m
@@ -0,0 +1,21 @@
+//
+//  fxjview.m
+//  FXJAutoLayoutAnimation
+//
+//  Created by myApplePro01 on 16/4/15.
+//  Copyright © 2016年 LSH. All rights reserved.
+//
+
+#import "fxjview.h"
+
+@implementation fxjview
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
